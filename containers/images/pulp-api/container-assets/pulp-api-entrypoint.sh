402ad40b8967245acbad64f5dcfb5d52f044713e
Mike DePaulo <mikedep333@redhat.com>
2019-05-06 16:07:09 -0400

    Fix/expand upon running migrations

--- containers/images/pulp-api/container-assets/pulp-api-entrypoint.sh
+++ containers/images/pulp-api/container-assets/pulp-api-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-source /pulp-common-entrypoint.sh
+export DJANGO_SETTINGS_MODULE=pulpcore.app.settings
 
-django-admin migrate --noinput
+#TODO: Determine list of installed plugins by inspecting image contents
+scl enable rh-python36 "django-admin makemigrations file ansible cookbook docker maven python"
+scl enable rh-python36 "django-admin migrate --noinput"
+scl enable rh-python36 "django-admin migrate auth --noinput"
 
 exec "$@"
